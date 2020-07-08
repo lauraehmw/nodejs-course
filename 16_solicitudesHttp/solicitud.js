f6816da9b999938f61cd9b97e1903b53b428846c
lauraehmw <lauraehmata@gmail.com>
2020-07-08 15:00:01 -0500

    Solicitud HTTP

--- 16_solicitudesHttp/solicitud.js
+++ 16_solicitudesHttp/solicitud.js
@@ -1,8 +1,28 @@
 const http = require('http');
 
-
+/* Hostname: The Wepage URL. Path... well. the path*/
 const options = {
-    hostname: 'localhost',
+    hostname: 'jsonplaceholder.typicode.com',
     port: 80,
-    path
+    path: '/posts',
+    method: 'GET'
 }
+
+const req = http.request(options, res => {
+    console.log(`status code: ${res.statusCode}`);
+    console.log(`headers: %j`, res.headers);
+
+    let body = ''
+    res.on('data', chunk =>{
+        body += chunk;
+    })
+
+    res.on('end', ()=>{
+        console.log('\n\n Resultados:');
+        console.log(body);
+    })
+});
+
+req.on('error', err=>{})
+
+req.end();
